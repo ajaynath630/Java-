0ebf42b9a29d4d5db49e35cfdeb8844471578f95
ajaynath630 <57582281+ajaynath630@users.noreply.github.com>
2021-07-10 19:31:50 +0530

    comit1

---
+++ JAVA/Quickstart.java
@@ -0,0 +1,20 @@
+
+class ThreadUsing extends Thread {
+
+public void run() {
+    Thread thread=new Thread();
+    thread.getPriority();
+
+
+
+}    
+  
+
+}
+
+
+class QuickStart {
+    public static void main(String[] args) {
+        System.out.println("Hello, World.");
+    }
+}
